2e44362a2c5820fc69102df89b29fb89bf00123f
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-05-14 19:45:07 +0600

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isExist(map<char, int>& m1, map<char, int>& m2)
+    {
+        for(auto& [u, v]: m2)
+        {
+            if(m1.find(u) != m1.end() && m1[u] >= v) continue;
+            return false;
+        }
+        return true;
+    }
+    string minWindow(string s, string t) {
+        
+        map<char, int> m1, m2;
+        
+        for(int i = 0; i < t.size(); i++) m2[t[i]]++;
+
+        int mnLen = 1e5;
+        pair<int, int> range = {-1, -1};
+        for(int l = 0, r = 0; r < s.size(); r++)
+        {
+            m1[s[r]]++;
+            while(isExist(m1, m2))
+            {
+                cout << l << " " << r << "\n";
+                if(r-l+1 < mnLen)
+                {
+                    mnLen = r-l+1;
+                    range = {l, r};
+                }
+                m1[s[l]]--;
+                l++;
+            }
+        }
+        string ans = "";
+        if(range.first != -1 && range.second != -1)
+        {
+            for(int i = range.first; i <= range.second; i++) ans += s[i];
+        }
+        return ans;
+    }
+};
