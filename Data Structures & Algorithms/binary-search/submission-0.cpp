492ec533d16914509fdedba3a7d5131435b4dd6a
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-05-15 10:06:01 +0600

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        auto it = lower_bound(nums.begin(), nums.end(), target);
+
+        if(*it != target) return -1;
+        else return it-nums.begin();
+    }
+};
