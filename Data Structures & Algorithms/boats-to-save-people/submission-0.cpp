73faf2de1d16982fd5eb189d46cf76a166d85ee2
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-05-21 15:41:01 +0600

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        
+        multiset<int> ms;
+        for(int i = 0; i < people.size(); i++) ms.insert(people[i]);
+        int cnt = 0;
+        while(ms.size() > 1)
+        {
+            cnt++;
+            auto it = ms.end();
+            it--;
+            auto it2 = ms.upper_bound(limit-*it);
+            ms.erase(it);
+            if(it2 != ms.begin())
+            {
+                it2--;
+                ms.erase(it2);
+            }
+        }
+        if(!ms.empty()) cnt++;
+        return cnt;        
+    }
+};
