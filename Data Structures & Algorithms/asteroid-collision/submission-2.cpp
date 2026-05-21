8940b3d92853d9debf2c40718619bf0c5716ff8f
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-05-21 17:58:13 +0600

    Add: asteroid-collision - submission-2

---
+++ Data Structures & Algorithms/asteroid-collision/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        
+        stack<int> st;
+        for(int i = 0; i < asteroids.size(); i++)
+        {
+            if(asteroids[i] > 0 || st.empty() || st.top() < 0) 
+            {
+                st.push(asteroids[i]);
+                continue;
+            }
+            while(!st.empty() && st.top() > 0 && st.top() < -1*asteroids[i]) st.pop();
+
+            if(st.empty() || st.top() < 0) st.push(asteroids[i]);
+            else if(st.top() == -1*asteroids[i]) st.pop();
+        }
+        vector<int> ans;
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
